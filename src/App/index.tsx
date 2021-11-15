cba86bf9033524ac29e792985898933dc76d15bc
Loke Sonne <lokesonne@lokes-macbook-pro.home>
2021-11-15 14:27:34 +0100

    Adds api request and table

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import UserTable from '../UserTable';
 import './style.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <UserTable />
       </header>
     </div>
   );
--- src/UserTable/index.tsx
+++ src/UserTable/index.tsx
@@ -1,13 +1,76 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './style.css';
 
-function App() {
+type User = {
+    id: number;
+    email: string;
+    first_name: string;
+    last_name: string;
+    avatar: string;
+}
+
+type UserResponse = {
+    page: number,
+    per_page: number,
+    total: number,
+    total_pages: number,
+    data: User[]
+}
+
+
+function UserTable() {
+
+const [users, setUsers] = useState<UserResponse>();
+
+useEffect(() => {
+
+    async function getUsers() {
+      const response = await fetch('https://reqres.in/api/users', {
+        method: 'GET',
+        mode: 'cors',
+        headers: {
+          'Content-Type': 'application/json'}
+      })
+      
+      if (response.status >= 200 && response.status <= 299) {
+        const jsonResponse = await response.json();
+        const userResponse: UserResponse = jsonResponse;
+        setUsers(userResponse);
+      } else {
+        // Handle errors
+        console.log(response.status, response.statusText);
+      }    
+    }
+    getUsers();
+},[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
+<table className="table table-dark table-hover">
+    <thead>
+    <tr>
+      <th scope="col">Id</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">email</th>
+    </tr>
+    </thead>
+          <tbody>
+            {
+              users?.data &&
+              users.data.map((user) => {
+                return (
+                  <tr key={user.id}>
+                    <td>{user.id}</td>
+                    <td>{user.first_name}</td>
+                    <td>{user.last_name}</td>
+                    <td>{user.email}</td>
+                  </tr>
+                )
+              })
+            }
+          </tbody>
+        </table>
   );
 }
 
-export default App;
+export default UserTable;
