73e71352ea188e0a82bd20a4a8ce31fb46c9bc77
Loke Sonne <lokesonne@lokes-macbook-pro.home>
2021-11-15 21:24:14 +0100

    Adds links to table

--- src/UserTable/index.tsx
+++ src/UserTable/index.tsx
@@ -3,21 +3,21 @@ import './style.css';
 import { Link, useParams } from "react-router-dom";
 import { UserResponse } from '../Models/User';
 
-function Pagination(props: {userResponse: UserResponse, currentPage: number}) {
+function Pagination(props: {userResponse: UserResponse, currentPage: number | undefined}) {
 
     let pages = [];
     if (props.userResponse.total_pages > 1) {
         for (let i = 0; i < props.userResponse.total_pages; i++) {           
-            pages.push(<li className="page-item" key={i}><Link className="page-link" to={`/users/${i+1}`}>{i+1}</Link></li>)
+            pages.push(<li className="page-item" key={i}><Link className="page-link link-pink" to={`/users/${i+1}`}>{i+1}</Link></li>)
         }
     }
 
     return (
         <nav aria-label="Page navigation example">
             <ul className="pagination">
-                <li className="page-item"><Link className="page-link" to={`/users/1`}>Previous</Link></li>
+                <li className="page-item"><Link className="page-link link-pink" to={`/users/1`}>Previous</Link></li>
                     {props.userResponse.total_pages > 1 && pages}
-                <li className="page-item"><Link className="page-link" to={`/users/${props.userResponse.total_pages}`}>Next</Link></li>
+                <li className="page-item"><Link className="page-link link-pink" to={`/users/${props.userResponse.total_pages}`}>Next</Link></li>
             </ul>
         </nav>
     )
@@ -62,14 +62,13 @@ useEffect(() => {
   </div>
   :
   <>
-<Pagination userResponse={users} currentPage={1} />
-<table className="table table-dark table-hover">
+<table className="table table-hover">
     <thead>
     <tr>
       <th scope="col">Id</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">email</th>
+      <th scope="col">Firstname</th>
+      <th scope="col">Lastname</th>
+      <th scope="col">Email</th>
     </tr>
     </thead>
           <tbody>
@@ -77,16 +76,22 @@ useEffect(() => {
               users.data.map((user) => {
                 return (
                   <tr key={user.id}>
-                    <td>{user.id}</td>
+                    <td><Link className="link-pink" to={`/user/${user.id}`}>{user.id}</Link></td>
                     <td>{user.first_name}</td>
                     <td>{user.last_name}</td>
                     <td>{user.email}</td>
-                  </tr>
+                  </tr>               
                 )
               })
             }
           </tbody>
         </table>
+        {
+            users.total_pages > 1 &&
+            <div className="d-flex justify-content-center">
+                <Pagination userResponse={users} currentPage={params.pagenumber ? parseInt(params.pagenumber) : undefined} />
+            </div>
+        }
         </>
   );
 }
